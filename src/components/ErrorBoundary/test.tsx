ac2dd009489b481a1ec77653211152dde14f11ff
Éverton Toffanetto <evertondgn@hotmail.com>
2022-02-07 01:15:46 -0300

    refactor: organize code

--- src/components/ErrorBoundary/test.tsx
+++ src/components/ErrorBoundary/test.tsx
@@ -2,14 +2,17 @@ import { screen } from '@testing-library/react'
 import { renderWithTheme } from 'utils'
 import ErrorBoundary from '.'
 
-const spy = jest.spyOn(console, 'error')
-spy.mockImplementation(() => {})
-
 const ChildWithError = () => {
   throw new Error()
 }
 
 describe('[Components] ErrorBoundary', () => {
+  let spy: jest.SpyInstance<void, [message?: any, ...optionalParams: any[]]>
+  beforeAll(() => {
+    spy = jest.spyOn(console, 'error').mockImplementation(() => {})
+  })
+  afterAll(() => spy.mockRestore())
+
   test('should render an error message', () => {
     renderWithTheme(
       <ErrorBoundary>
