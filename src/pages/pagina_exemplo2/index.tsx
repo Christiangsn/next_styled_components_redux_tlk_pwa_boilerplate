b5a2768ad268dadfc91dc413ad0b1133f5a49842
Éverton Toffanetto <evertondgn@hotmail.com>
2022-02-07 13:58:49 -0300

    refactor: remove logic from page component

--- src/pages/pagina_exemplo2/index.tsx
+++ src/pages/pagina_exemplo2/index.tsx
@@ -1,19 +1,10 @@
-import { useCallback } from 'react'
 import { useRouter } from 'next/router'
-import { useWindowSize } from 'hooks'
 import { LayoutExample } from 'layouts'
 import * as S from './styles'
 import * as C from 'components'
 
 export default function PageExample2() {
   const router = useRouter()
-  const { windowSize } = useWindowSize()
-
-  const renderTextByBreakpoint = useCallback(() => {
-    if (windowSize.md) return 'Breakpoint Desktop, "md": 1024px - ∞px'
-    if (windowSize.sm) return 'Breakpoint Tablet, "sm": 600px - 1023px'
-    if (windowSize.xs) return 'Breakpoint Mobile, "xs": 0px - 599px'
-  }, [windowSize.md, windowSize.sm, windowSize.xs])
 
   return (
     <S.Container as="section">
@@ -23,14 +14,7 @@ export default function PageExample2() {
 
       <br />
 
-      <h3>
-        Redimencione a janela do navegador para visualizar o texto renderizado
-        para cada breakpoint!
-      </h3>
-
-      <br />
-
-      <h1>{renderTextByBreakpoint()}</h1>
+      <C.RenderTextByBreakpoint />
 
       <S.Column>
         <C.Button
