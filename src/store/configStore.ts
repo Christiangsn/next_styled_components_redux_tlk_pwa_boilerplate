ef18cc1aacad6d37e46830c27e1c28eaf198efbe
Éverton Toffanetto <evertondgn@hotmail.com>
2022-04-17 02:23:24 -0300

    refactor: move redux persist logic to separate file

--- src/store/configStore.ts
+++ src/store/configStore.ts
@@ -1,31 +1,19 @@
 import { TypedUseSelectorHook, useSelector } from 'react-redux'
-import { combineReducers, configureStore } from '@reduxjs/toolkit'
+import { configureStore } from '@reduxjs/toolkit'
 import {
   FLUSH,
   PAUSE,
   PERSIST,
-  persistReducer,
   persistStore,
   PURGE,
   REGISTER,
   REHYDRATE
 } from 'redux-persist'
-import asyncSessionStorage from 'redux-persist/lib/storage'
-import { changeExample } from './slices'
-import { RootState } from './types'
+import { persistedReducer } from './persistConfig'
+import { RootState } from './rootState'
 
 export const isDevelopment = process.env.NODE_ENV === 'development'
 export const useTypedSelector: TypedUseSelectorHook<RootState> = useSelector
-
-const reducer = combineReducers({ changeExample })
-
-const persistConfig = {
-  key: 'root',
-  storage: asyncSessionStorage,
-  whitelist: ['changeExample']
-}
-
-const persistedReducer = persistReducer(persistConfig, reducer)
 
 export const store = configureStore({
   reducer: persistedReducer,
