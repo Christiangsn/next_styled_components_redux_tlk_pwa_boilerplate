bba411545433d2dea6de5160203e00a91022a57c
Éverton Toffanetto <evertondgn@hotmail.com>
2022-04-17 23:39:52 -0300

    refactor: change name

--- src/templates/Default/index.tsx
+++ src/templates/Default/index.tsx
@@ -4,7 +4,7 @@ import { DefaultProps } from './types'
 export const Default = ({ children }: DefaultProps) => {
   return (
     <S.Grid>
-      <h1>Layout Default</h1>
+      <h1>Template Default</h1>
       <div>{children}</div>
     </S.Grid>
   )
