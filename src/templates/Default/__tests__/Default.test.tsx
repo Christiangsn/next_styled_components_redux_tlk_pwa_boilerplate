c5daf38d32c5ef15db78eca3f1af5b7c2a6dbbda
Éverton Toffanetto <evertondgn@hotmail.com>
2022-05-02 10:47:20 -0300

    test: refactor tests

--- src/templates/Default/__tests__/Default.test.tsx
+++ src/templates/Default/__tests__/Default.test.tsx
@@ -3,13 +3,17 @@ import { Default } from '..'
 import { renderWithProviders } from 'utils'
 
 describe('[Template] Default', () => {
-  it('should render a children', () => {
+  it('should render a children and a title', () => {
     renderWithProviders(
       <Default>
-        <h1 data-testid="select_template">Test Template</h1>
+        <p data-testid="select_template">Test Template</p>
       </Default>
     )
 
-    expect(screen.getByTestId('select_template')).toBeInTheDocument()
+    const title = screen.getByRole('heading', { name: 'Template Default' })
+    const children = screen.getByTestId('select_template')
+
+    expect(children).toBeInTheDocument()
+    expect(title).toBeInTheDocument()
   })
 })
