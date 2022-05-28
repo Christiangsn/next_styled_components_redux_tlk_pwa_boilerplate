15383981075047973cdd2b49fc9b07cd60389405
Éverton Toffanetto <evertondgn@hotmail.com>
2022-05-28 04:46:06 -0300

    chore: config jest

--- jest.config.js
+++ jest.config.js
@@ -1,6 +1,9 @@
 module.exports = {
   testEnvironment: 'jest-environment-jsdom',
   collectCoverage: true,
+  fakeTimers: {
+    enableGlobally: true
+  },
   testPathIgnorePatterns: ['/node_modules', '/.next/'],
   moduleDirectories: ['node_modules', '<rootDir>/'],
   setupFilesAfterEnv: ['<rootDir>/.jest/setup.ts'],
@@ -11,8 +14,10 @@ module.exports = {
     'src/pages/index.page.tsx',
     'src/pages/_document.page.tsx',
     'src/components/index.tsx',
+    'src/components/atoms/index.tsx',
+    'src/components/molecules/index.tsx',
+    'src/components/templates/index.tsx',
     'src/hooks/index.ts',
-    'src/templates/index.ts',
     'utils/providers',
     'src/utils/index.tsx',
     'src/types',
@@ -31,6 +36,14 @@ module.exports = {
   },
   moduleNameMapper: {
     '^.+\\.svg$': '<rootDir>/src/mocks/svg.ts'
+  },
+  coverageThreshold: {
+    global: {
+      branches: 85,
+      functions: 85,
+      lines: 85,
+      statements: -10
+    }
   }
 }
 
@@ -52,8 +65,10 @@ module.exports = {
 //  'src/pages/index.page.tsx',
 //  'src/pages/_document.page.tsx',
 //  'src/components/index.tsx',
+//  'src/components/atoms/index.tsx',
+//  'src/components/molecules/index.tsx',
+//  'src/components/templates/index.tsx',
 //  'src/hooks/index.ts',
-//  'src/templates/index.ts',
 //  'utils/providers',
 //  'src/utils/index.ts',
 //  'src/types',
